de85ecffba5d3babe8af7e3d782f21f89445d8a3
nisahakimi <nisahakimi99@gmail.com>
2024-09-15 22:47:18 +0700

    Update schedule_screen.dart
    Change hari to checkbox and allow multiple hari with same waktu mulai and waktu akhir

--- lib/schedule_screen.dart
+++ lib/schedule_screen.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:kidztime/model/jadwalPenggunaan.dart';
 import 'package:kidztime/page/widget/card_widget.dart';
-import 'package:kidztime/page/widget/dropdown_input_widget.dart';
 import 'package:kidztime/page/widget/header_widget.dart';
-import 'package:kidztime/page/widget/radio_input_widget.dart';
 import 'package:kidztime/page/widget/time_input_widget.dart';
+import 'package:kidztime/page/widget/radio_input_widget.dart';
 import 'package:kidztime/utils/colors.dart';
 import 'package:kidztime/utils/database.dart';
 import 'package:kidztime/utils/widget_util.dart';
@@ -16,9 +14,6 @@ import 'package:sqflite/sqflite.dart';
 class ScheduleScreen extends StatefulWidget {
   ScheduleScreen({super.key});
 
-  late TextEditingController waktuMulaiController = TextEditingController();
-  late TextEditingController waktuAkhirController = TextEditingController();
-
   late Future<Database> dbKidztime = DBKidztime().getDatabase();
 
   @override
@@ -26,8 +21,7 @@ class ScheduleScreen extends StatefulWidget {
 }
 
 class _ScheduleScreenState extends State<ScheduleScreen> {
-  bool _selectedStatus = true;
-  String? selectedDay;
+  ScheduleEntry _schedule = ScheduleEntry();
 
   @override
   void initState() {
@@ -58,107 +52,32 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Text("Please set a gadget usage schedule"),
-                    CardWidget(
-                      verticalMargin: 30.0,
-                      horizontalMargin: 30.0,
-                      verticalPadding: 40.0,
-                      horizontalPadding: 30.0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          DropdownInputWidget(
-                            title: 'Pilih Hari',
-                            items: const [
-                              'Senin',
-                              'Selasa',
-                              'Rabu',
-                              'Kamis',
-                              'Jumat',
-                              'Sabtu',
-                              'Minggu'
-                            ],
-                            value: selectedDay,
-                            onChanged: (value) {
-                              setState(() {
-                                selectedDay = value;
-                              });
-                            },
+                    _buildScheduleForm(), // Only one schedule form now
+                    const SizedBox(height: 20),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: InkWell(
+                        onTap: () {
+                          _saveSchedule(); // Save the single schedule
+                        },
+                        borderRadius: BorderRadius.circular(10.0),
+                        splashColor: Colors.amber,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 40.0, vertical: 5.0),
+                          decoration: BoxDecoration(
+                            color: WidgetUtil().parseHexColor(darkColor),
+                            borderRadius: BorderRadius.circular(10.0),
                           ),
-                          TimeInputWidget(
-                            title: "Waktu Mulai",
-                            controller: widget.waktuMulaiController,
-                            hint: "Tekan di sini",
-                            initialTime: widget.waktuMulaiController.text == ""
-                                ? TimeOfDay.now()
-                                : TimeOfDay(
-                                    hour: int.parse(widget
-                                        .waktuMulaiController.text
-                                        .split(":")[0]),
-                                    minute: int.parse(widget
-                                        .waktuMulaiController.text
-                                        .split(":")[1])),
-                          ),
-                          const SizedBox(
-                            height: 10.0,
-                          ),
-                          TimeInputWidget(
-                            title: "Waktu Akhir",
-                            controller: widget.waktuAkhirController,
-                            hint: "Tekan di sini",
-                            initialTime: widget.waktuAkhirController.text == ""
-                                ? TimeOfDay.now()
-                                : TimeOfDay(
-                                    hour: int.parse(widget
-                                        .waktuAkhirController.text
-                                        .split(":")[0]),
-                                    minute: int.parse(widget
-                                        .waktuAkhirController.text
-                                        .split(":")[1])),
-                          ),
-                          const SizedBox(
-                            height: 10.0,
-                          ),
-                          RadioButtonWidget<bool>(
-                            title: "Status Aktif",
-                            options: const [true, false],
-                            optionLabels: const {
-                              true: 'Aktif',
-                              false: 'Tidak Aktif',
-                            },
-                            groupValue: _selectedStatus,
-                            onChanged: (value) {
-                              setState(() {
-                                _selectedStatus = value ?? true;
-                              });
-                            },
-                          ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: InkWell(
-                              onTap: () {
-                                _saveSchedule();
-                              },
-                              borderRadius: BorderRadius.circular(10.0),
-                              splashColor: Colors.amber,
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 40.0, vertical: 5.0),
-                                decoration: BoxDecoration(
-                                  color: WidgetUtil().parseHexColor(darkColor),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: const Text(
-                                  "Simpan",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
+                          child: const Text(
+                            "Simpan Jadwal",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ],
+                        ),
                       ),
                     ),
                   ],
@@ -171,127 +90,147 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
     );
   }
 
-  void _saveSchedule() async {
-    if (validateInput()) {
-      Database db = await widget.dbKidztime;
-      WidgetUtil().customeDialog(
-        context: context,
-        title: "Simpan data?",
-        detail: [
-          const Text("Simpan data jadwal penggunaan gadget?"),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                width: 100,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Hari"),
-                    Text("Waktu Mulai"),
-                    Text("Waktu Akhir"),
-                    Text("Status Aktif"),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      selectedDay ?? "",
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      widget.waktuMulaiController.text,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      widget.waktuAkhirController.text,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      _selectedStatus ? "Aktif" : "Tidak Aktif",
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+  Widget _buildScheduleForm() {
+    return CardWidget(
+      verticalMargin: 30.0,
+      horizontalMargin: 30.0,
+      verticalPadding: 40.0,
+      horizontalPadding: 30.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Pilih Hari'),
+          Column(
+            children: _schedule.selectedDays.keys.map((day) {
+              return CheckboxListTile(
+                title: Text(day),
+                value: _schedule.selectedDays[day],
+                onChanged: (value) {
+                  setState(() {
+                    _schedule.selectedDays[day] = value ?? false;
+                  });
+                },
+              );
+            }).toList(),
+          ),
+          TimeInputWidget(
+            title: "Waktu Mulai",
+            controller: _schedule.waktuMulaiController,
+            hint: "Tekan di sini",
+            initialTime: _schedule.getWaktuMulai(),
+          ),
+          const SizedBox(height: 10.0),
+          TimeInputWidget(
+            title: "Waktu Akhir",
+            controller: _schedule.waktuAkhirController,
+            hint: "Tekan di sini",
+            initialTime: _schedule.getWaktuAkhir(),
+          ),
+          const SizedBox(height: 10.0),
+          RadioButtonWidget<bool>(
+            title: "Status Aktif",
+            options: const [true, false],
+            optionLabels: const {true: 'Aktif', false: 'Tidak Aktif'},
+            groupValue: _schedule.selectedStatus,
+            onChanged: (value) {
+              setState(() {
+                _schedule.selectedStatus = value ?? true;
+              });
+            },
           ),
         ],
-        okButtonFunction: () async {
-          // Create Jadwalpenggunaan instance
+      ),
+    );
+  }
+
+  void _saveSchedule() async {
+    Database db = await widget.dbKidztime;
+
+    if (_schedule.isValid()) {
+      Set<String> processedDays = {};
+
+      for (var day in _schedule.selectedDays.keys) {
+        if (_schedule.selectedDays[day] == true &&
+            !processedDays.contains(day)) {
           Jadwalpenggunaan jadwalpenggunaan = Jadwalpenggunaan(
-            hari: selectedDay ?? "",
-            waktuMulai: widget.waktuMulaiController.text,
-            waktuAkhir: widget.waktuAkhirController.text,
-            statusAktif: _selectedStatus,
+            hari: day,
+            waktuMulai: _schedule.waktuMulaiController.text,
+            waktuAkhir: _schedule.waktuAkhirController.text,
+            statusAktif: _schedule.selectedStatus,
           );
 
-          // Check if a record with the same 'hari' exists
-          var existingJadwal =
-              await getJadwalByHari(widget.dbKidztime, selectedDay);
+          var existingJadwal = await getJadwalByHari(widget.dbKidztime, day);
 
           if (existingJadwal != null) {
-            // If the record exists, update the existing entry
-            jadwalpenggunaan.id = existingJadwal.id; // Ensure id is mutable
+            jadwalpenggunaan.id = existingJadwal.id;
             await updateJadwalPenggunaan(widget.dbKidztime, jadwalpenggunaan);
             print('Data updated in database: $jadwalpenggunaan');
           } else {
-            // If no record exists, insert a new entry and get the ID
             int newId = await insertJadwalPenggunaan(
                 widget.dbKidztime, jadwalpenggunaan);
-            jadwalpenggunaan.id = newId; // Update model with the new ID
+            jadwalpenggunaan.id = newId;
             print('New data added to database: $jadwalpenggunaan');
           }
 
-          // Close the dialog and show success message
+          processedDays.add(day);
+
+          print('Schedule for $day:');
+          print('  Waktu Mulai: ${_schedule.waktuMulaiController.text}');
+          print('  Waktu Akhir: ${_schedule.waktuAkhirController.text}');
+          print('  Status Aktif: ${_schedule.selectedStatus}');
+        }
+      }
+
+      WidgetUtil().customeDialog(
+        context: context,
+        title: "Berhasil",
+        detail: [const Text("Semua data berhasil disimpan")],
+        okButtonText: "OK",
+        okButtonFunction: () {
           Navigator.of(context).pop();
-          WidgetUtil().showLoadingDialog(
-            context: context,
-          );
-          Timer(const Duration(seconds: 3), () {
-            Navigator.of(context).pop();
-            WidgetUtil().customeDialog(
-              context: context,
-              title: "Berhasil",
-              detail: [const Text("Data berhasil disimpan")],
-              okButtonText: "OK",
-              okButtonFunction: () {
-                Navigator.of(context).pop();
-                Get.offAndToNamed('/main-menu');
-              },
-            );
-          });
+          Get.offAndToNamed('/main-menu');
         },
-        okButtonText: "Simpan",
-        cancelButtonText: "Batal",
       );
+    } else {
+      WidgetUtil().showToast(msg: "Invalid input for schedule entry!");
     }
   }
+}
 
-  bool validateInput() {
-    if (selectedDay == null ||
-        widget.waktuMulaiController.text.isEmpty ||
-        widget.waktuAkhirController.text.isEmpty) {
-      WidgetUtil().showToast(msg: "Harap isi semua kolom!");
-      return false;
-    }
-    if (widget.waktuMulaiController.text.length < 5 ||
-        widget.waktuAkhirController.text.length < 5) {
-      WidgetUtil().showToast(msg: "Format waktu salah!");
-      return false;
-    }
-    if (widget.waktuMulaiController.text.isEmpty) {
-      WidgetUtil().showToast(msg: "Waktu mulai tidak boleh kosong!");
-      return false;
-    }
-    if (widget.waktuAkhirController.text.isEmpty) {
-      WidgetUtil().showToast(msg: "Waktu akhir tidak boleh kosong!");
-      return false;
-    }
+class ScheduleEntry {
+  TextEditingController waktuMulaiController = TextEditingController();
+  TextEditingController waktuAkhirController = TextEditingController();
+  bool selectedStatus = true;
+  Map<String, bool> selectedDays = {
+    'Senin': false,
+    'Selasa': false,
+    'Rabu': false,
+    'Kamis': false,
+    'Jumat': false,
+    'Sabtu': false,
+    'Minggu': false,
+  };
 
-    return true;
+  TimeOfDay getWaktuMulai() {
+    return waktuMulaiController.text.isEmpty
+        ? TimeOfDay.now()
+        : TimeOfDay(
+            hour: int.parse(waktuMulaiController.text.split(":")[0]),
+            minute: int.parse(waktuMulaiController.text.split(":")[1]),
+          );
+  }
+
+  TimeOfDay getWaktuAkhir() {
+    return waktuAkhirController.text.isEmpty
+        ? TimeOfDay.now()
+        : TimeOfDay(
+            hour: int.parse(waktuAkhirController.text.split(":")[0]),
+            minute: int.parse(waktuAkhirController.text.split(":")[1]),
+          );
+  }
+
+  bool isValid() {
+    return waktuMulaiController.text.isNotEmpty &&
+        waktuAkhirController.text.isNotEmpty;
   }
 }
